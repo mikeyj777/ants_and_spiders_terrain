52eeab3d03049c3ceb16adb21b1476310d3502a5
Michael James <mikeyj777@gmail.com>
2024-10-02 22:58:53 -0400

    scene without box

--- client/src/components/Scene.js
+++ client/src/components/Scene.js
@@ -7,32 +7,6 @@ import ForestModel from './ForestModel';
 import CameraControls from './CameraControls';
 import { agentToModelCoordinates } from '../utils/transform';
 
-// Adjustable variables
-const BOX_ROTATION_DEGREES = 35;
-const BOX_SIZE = 45;
-const BOX_HEIGHT = 10;
-const BOX_ROTATION = BOX_ROTATION_DEGREES * (Math.PI / 180); // in radians
-const LOWEST_ELEVATION = -5;
-const ANCHOR_POINT = new Vector3(-7, LOWEST_ELEVATION, -30); // Bottom corner of the box
-
-const AlignedBoundingBox = ({ size, height, rotation, anchorPoint }) => {
-  const halfSize = size / 2;
-  const boxCenter = new Vector3(
-    anchorPoint.x + halfSize,
-    anchorPoint.y + height / 2,
-    anchorPoint.z + halfSize
-  );
-
-  return (
-    <mesh 
-      position={boxCenter} 
-      rotation={new Euler(0, rotation, 0)}
-    >
-      <boxGeometry args={[size, height, size]} />
-      <meshBasicMaterial color="red" wireframe={true} />
-    </mesh>
-  );
-};
 
 const Scene = () => {
   const [agentPosition, setAgentPosition] = useState(new Vector3(100, 10, 0));
@@ -68,13 +42,6 @@ const Scene = () => {
         <sphereGeometry args={[0.5]} />
         <meshStandardMaterial color="red" />
       </mesh>
-
-      <AlignedBoundingBox 
-        size={BOX_SIZE} 
-        height={BOX_HEIGHT} 
-        rotation={BOX_ROTATION} 
-        anchorPoint={ANCHOR_POINT}
-      />
 
       <CameraControls />
     </>
